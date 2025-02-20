e4dc72e0a24c9c924d3008e6429498abd9e4dd12
MinJunChoi <chlalswns200@naver.com>
2025-02-20 16:48:09 +0900

    add internal call alter 2

---
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,27 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV2 {
+
+    private final ObjectProvider<CallServiceV2> callServiceProvider; // 지연(LAZY) 조회
+
+    public void external() {
+        log.info("call external");
+
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+
+
+}
